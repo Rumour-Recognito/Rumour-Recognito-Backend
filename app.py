3603d543a145d9dbc44a2a8bd9cdac31f8de1ef1
subhamb04 <subham040800@gmail.com>
2022-05-12 18:01:21 +0530

    removed routes...added image processing

--- app.py
+++ app.py
@@ -10,35 +10,37 @@ from search_google import *
 from pred import *
 from flask_cors import CORS
 from sentimental_analysis import *
+from image_process import *
+
 
 app = Flask(__name__)
 CORS(app)
 base_dir = './'
 ### Check if server is suning successfully ###
 
-
 @app.route('/')
 def hello_world():
     return 'Rumour-Reckon Backend running!'
 
 # Provides the current status of the analysis
-
-
 status = 0
-
 
 @app.route('/status')
 def getStatus():
+    print(status)
     return str(status)
 
 ### Individual tweet details (without comments)  ###
-
 
 @app.route('/tweet-scrape')
 def scrape_twitter():
     id = request.args.get('id')
     tweet = getTweet(id)
-    return tweet
+    print(tweet)
+    print("Image analysis: ")
+    print(analyze_image(tweet['media'][0]['image_url']))
+    result = process_data(tweet['text'])
+    return result
 
 
 @app.route('/tweet-comments-scrape')
@@ -46,41 +48,131 @@ def scrape_twitter_comments():
     id = request.args.get('id')
     comments = getTweetComments(id)
     return jsonify(comments)
-
+    
 
 @app.route('/facebook-scrape')
 def scrape_facebook():
     id = request.args.get('id')
     post = scrapePosts(id)
-    return post
+    print(post)
+    print("Image analysis: ")
+    print(analyze_image(post['image']))
+    result = process_data(post['post_text'])
+
+    return result
+
+@app.route('/url-scrape', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        data = request.get_json()
+        content = process_input(data['link'])
+        return content
+    else:
+        return "Something went wrong"
 
 
-@app.route('/translate')
-def translate_text():
-    text = request.args.get('text')
-    translated_text = translate_to_english(text)
-    return translated_text
+def process_data(text):
+    sentences = text.split('\n')
+
+    link_removed_sentences = []
+    for sentence in sentences:
+        filteredText = removeLinksAndGetSentences(sentence)
+        link_removed_sentences.append(filteredText)
+
+    special_char_removed_sentences = []
+    for sentence in link_removed_sentences:
+        filteredText = filterTextFromSpecialCharacters(sentence)
+        special_char_removed_sentences.append(filteredText)
+
+    
+    filtered_sentences = removeEmptySentence(special_char_removed_sentences)
+
+    global status
+    status = 0
+
+    translated_sentences = []
+
+    for sentence in filtered_sentences:
+        sentence = translate_to_english(sentence)
+        translated_sentences.append(sentence)
+    
+    status = 1
+    result = getSimilarNews(translated_sentences)
+    return result
+
+# function to remove links from sentences
+def removeLinksAndGetSentences(text):
+    wordsInText = text.split(' ')
+    filteredWord = []
+
+    for word in wordsInText:
+        if ('http' not in word and 'www' not in word):
+            filteredWord.append(word)
+
+    return ' '.join(filteredWord)
+
+# function to filterTextFromSpecialCharacters
+def filterTextFromSpecialCharacters(text):
+    text = text.strip()
+    filteredText = []
+    specialChars = '/*!@#$%^&*()"{}_[]|\\?/<>,.'
+
+    for letter in text:
+        if(letter in specialChars):
+            filteredText.append(' ')
+        else:
+            filteredText.append(letter)
+
+    return ''.join(filteredText).strip()
 
 
-@app.route('/search-google-links')
-def google_search_links():
-    query = request.args.get('query')
-    limit = request.args.get('limit')
-    results = search_google(query, limit)
-    return results
+# remove empty space
+def removeEmptySentence(sentences):
+    nonEmptySentences = []
+    for sentence in sentences:
+        if sentence != '':
+            nonEmptySentences.append(sentence)
 
+    return nonEmptySentences
 
-@app.route('/search-google-content')
-def google_search_content():
-    query = request.args.get('query')
-    limit = request.args.get('limit')
+def getSimilarNews(sentences):
+    counter = 1
+    bodies = []
+    bodiesColumnTitles = ['Body ID', 'articleBody']
+    headings = []
+    headingsColumnTitles = ['Headline', 'Body ID']
 
-    allContents = google_search_content(query, limit)
+    bodiesContent = []
+    headlinesContent = []
+    for sentence in sentences:
+        allContent = google_search_content(sentence, 5)
+
+        for content in allContent:
+            if(content):
+                bodiesContent.append(content)
+                headlinesContent.append(sentence)
+                bodies.append([counter, content])
+                headings.append([sentence, counter])
+                counter += 1
+
+    with open('./data/test_bodies.csv', 'w', encoding="utf-8") as f:
+        write = csv.writer(f)
+        write.writerow(bodiesColumnTitles)
+        write.writerows(bodies)
+
+    with open('./data/test_stances_unlabeled.csv', 'w', encoding="utf-8") as f:
+        write = csv.writer(f)
+        write.writerow(headingsColumnTitles)
+        write.writerows(headings)
+
     dict = {}
-    dict['data'] = allContents
+    dict['heading'] = headings
+    dict['body'] = bodies
 
-    return dict
-
+    global status
+    status = 2
+    result = stance_detect(bodiesContent, headlinesContent)
+    return result
 
 def google_search_content(query, limit):
 
@@ -93,61 +185,8 @@ def google_search_content(query, limit):
 
     return allContents
 
-
-@app.route('/url-scrape', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        data = request.get_json()
-        content = process_input(data['link'])
-        return content
-    else:
-        return "Something went wrong"
-
-
-@app.route('/query-list', methods=['POST'])
-def sendQuery():
-    if request.method == 'POST':
-        data = request.get_json()
-
-        counter = 1
-        bodies = []
-        bodiesColumnTitles = ['Body ID', 'articleBody']
-        headings = []
-        headingsColumnTitles = ['Headline', 'Body ID']
-
-        sentences = data['queryList']
-        for sentence in sentences:
-            allContent = google_search_content(sentence, 5)
-
-            for content in allContent:
-                bodies.append([counter, content])
-                headings.append([sentence, counter])
-                counter += 1
-
-        with open('./data/test_bodies.csv', 'w', encoding="utf-8") as f:
-            write = csv.writer(f)
-            write.writerow(bodiesColumnTitles)
-            write.writerows(bodies)
-
-        with open('./data/test_stances_unlabeled.csv', 'w', encoding="utf-8") as f:
-            write = csv.writer(f)
-            write.writerow(headingsColumnTitles)
-            write.writerows(headings)
-
-        dict = {}
-        dict['heading'] = headings
-        dict['body'] = bodies
-
-        return dict
-    else:
-        return "Something went wrong"
-
-
-@app.route('/execute')
-def stance_detect():
-
+def stance_detect(bodies, headlines):
     load()
-
     predictions = []
     with open('./result/predictions_test.csv', newline='') as f:
         reader = csv.reader(f)
@@ -155,28 +194,6 @@ def stance_detect():
         for each in data:
             predictions.extend(each)
         predictions.remove("Prediction")
-
-    bodies = []
-    with open('./data/test_bodies.csv', newline='', encoding="utf-8") as f:
-        reader = csv.reader(f)
-        data = list(reader)
-
-        for each in data:
-            if(len(each) == 2):
-                bodies.append(each[1])
-
-        bodies.remove("articleBody")
-
-    headlines = []
-    with open('./data/test_stances_unlabeled.csv', newline='', encoding="utf-8") as f:
-        reader = csv.reader(f)
-        data = list(reader)
-
-        for each in data:
-            if(len(each) == 2):
-                headlines.append(each[0])
-
-        headlines.remove("Headline")
 
     score = sentimental_analysis(predictions, headlines, bodies)
 
@@ -187,6 +204,9 @@ def stance_detect():
             score -= 1
 
     print(score)
+    
+    global status
+    status = 3
 
     if(score > 0):
         return "LOOKS REAL"
---
+++ image_process.py
@@ -0,0 +1,27 @@
+import requests
+import pytesseract
+import cv2
+
+def analyze_image(url):
+    pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+    file_name = "image.jpg"
+
+    res = requests.get(url, stream = True)
+
+    if res.status_code == 200:
+        with open(file_name,'wb') as f:
+            f.write(requests.get(url).content)
+        print('Image sucessfully Downloaded: ',file_name)
+    else:
+        print('Image Couldn\'t be retrieved')
+
+    img = cv2.imread("image.jpg")
+    (h, w) = img.shape[:2]
+    img = cv2.resize(img, (w*3, h*3))
+    gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    thr = cv2.threshold(gry, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    text = pytesseract.image_to_string(thr)
+
+    return text
